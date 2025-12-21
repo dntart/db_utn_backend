aa39ea5e5f06f51e9c6f5d58b882d826f7e164c1
dntart <dantecesarsegovia@gmail.com>
2025-12-21 10:15:28 -0800

    agregue ruta auth/login en authroutes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -6,9 +6,10 @@ const authRouter = Router()  //se comienza conteniendo el llamado de la funcion(
 
 //TODAS LAS PETICIONES QUE LLEGAN AL authRouter empiezan con http://localhost:3000/auth
 
-
+http://localhost:3000/auth/register
 authRouter.post("/register", AuthController.authRegister)  //en la ruta va la ruta y la invocacion al controller, no solo la ruta
 
+http://localhost:3000/auth/login
 authRouter.post("/login", AuthController.authLogin)//en la ruta va la ruta y la invocacion al controller, no solo la ruta
 
 export default authRouter
